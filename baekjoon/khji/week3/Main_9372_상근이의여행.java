0d316806e4f1e45463df5a7275308453334e5ba5
khji <gkh4302@naver.com>
2022-08-24 00:56:28 +0900

    code : BOJ 9372 S4 상근이의여행

---
+++ baekjoon/khji/week3/Main_9372_상근이의여행.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main_9372_상근이의여행 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int t=1; t<=T; t++) {
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			for(int i=0; i<M; i++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+			}
+			System.out.println(N-1);
+		}
+	}
+}
